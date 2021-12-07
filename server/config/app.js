3db26775bbeb2b5198653455329398122b1316e6
anusoruban <34101007+anusasharma@users.noreply.github.com>
2021-12-07 20:02:18 +0530

    Update app.js

--- server/config/app.js
+++ server/config/app.js
@@ -42,8 +42,11 @@ let app = express();
 
 app.use(cors())
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, '../views'));
+// app.set('view engine', 'ejs');
+app.get('/', async (req, res, next) => {
+  res.send('Hello Survey')
+})
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -94,24 +97,24 @@ let strategy = new JWTStrategy(jwtOptions, (jwt_payload, done) => {
 
 passport.use(strategy);
 
-app.use('/', indexRouter);
+app.use('/auth', indexRouter);
 app.use('/users', usersRouter);
 app.use('/surveylist', surveyRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error',{title: 'Error'});
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error',{title: 'Error'});
+// });
 
 module.exports = app;
